0a86534584d49a40f1c5d18385249bc9a6a38b28
JOSÈ RODRIGUEZ <112988259+jos3376@users.noreply.github.com>
2023-10-19 18:03:52 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+let edad = prompt("Estás por ingresar a una página de ventas, ingresá tu edad ");
+if (parseInt(edad)>= 18){
+    alert ("sos mayor, puedes ingresar al sitio y realizar compras")
+}
+else{
+    alert ("sos menor, puedes ingresar pero no realizar compras")
+}
